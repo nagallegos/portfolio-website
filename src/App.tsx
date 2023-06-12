be5a3ab4bf5584ba4949dcf4082f8a35f3b45685
nagallegos <113269909+nagallegos@users.noreply.github.com>
2023-06-12 15:55:17 -0500

    Setup the initial layout of the site

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Grid
+      templateAreas={{
+        base: `"header" 
+               "main"
+               "footer"`,
+        lg: `"header header"
+             "nav main"
+             "nav footer"`,
+      }}
+      templateColumns={{
+        base: "1fr",
+        lg: "100px 1fr",
+      }}
+      templateRows={{
+        base: "50px 100px 30px", // change the 100px later
+        lg: "50px 100px 30px", // change the 100px later
+      }}
+    >
+      <GridItem area="header" bg="green">
+        Header
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="nav" bg="red">
+          Nav
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="blue">
+        Main
+      </GridItem>
+      <GridItem area="footer" bg="yellow">
+        Footer
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
