c5207d7d7df7e98aca8c035d942d13882130580f
nagallegos <113269909+nagallegos@users.noreply.github.com>
2023-06-13 13:41:26 -0500

    Got rid of amplify file for the moment

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+
 import { ChakraProvider, ColorModeScript, theme } from "@chakra-ui/react";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
