925a056d343308f3cc7eee376461ade894a8154f
nagallegos <113269909+nagallegos@users.noreply.github.com>
2023-06-13 14:26:23 -0500

    Added strict mode again...

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,10 @@ import "./index.css";
 import { ChakraProvider, ColorModeScript, theme } from "@chakra-ui/react";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <ChakraProvider theme={theme}>
-    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-    <App />
-  </ChakraProvider>
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
 );
